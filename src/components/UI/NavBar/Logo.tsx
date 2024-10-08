05a641d53f010e7d8e76b4e5b3897db5dad91809
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-10-08 16:45:20 +0100

    Init project.

---
+++ src/components/UI/NavBar/Logo.tsx
@@ -0,0 +1,11 @@
+import { RiNodejsLine } from 'react-icons/ri';
+
+const Logo = () => {
+  return (
+    <a href="/">
+      <RiNodejsLine className="text-secondary" size={60} />
+    </a>
+  );
+};
+
+export default Logo;

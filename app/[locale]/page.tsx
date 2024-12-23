c46c38cd169a7364a22f3b998e9caececc2e73fc
EngenMe <mohamdfarouk727@gmail.com>
2024-12-23 19:40:31 +0000

    Set the color style.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,8 +5,8 @@ export default function HomePage() {
 
   return (
     <div>
-      <h1>{t("hello")}</h1>
-      <h2>{t("welcome")}</h2>
+      <h1 className="text-muted-foreground">{t("hello")}</h1>
+      <h2 className="text-input">{t("welcome")}</h2>
     </div>
   );
 }

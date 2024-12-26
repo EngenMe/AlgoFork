bf4b5f017d8b1fdf0673ed724b93e6a33bf02af6
EngenMe <mohamdfarouk727@gmail.com>
2024-12-26 18:58:18 +0000

    Add multilanguage to partners.

--- components/Partners/Header.tsx
+++ components/Partners/Header.tsx
@@ -1,6 +1,10 @@
+import { useTranslations } from "next-intl";
+
 const Header = () => {
+  const t = useTranslations();
+
   return (
-    <h1 className="text-3xl font-bold text-center">Top Company Questions</h1>
+    <h1 className="text-3xl font-bold text-center">{t("partnersHeader")}</h1>
   );
 };
 export default Header;

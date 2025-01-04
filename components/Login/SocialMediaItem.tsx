0854ebb54c5e561f178bf35a7e76940bea560616
EngenMe <mohamdfarouk727@gmail.com>
2025-01-04 22:39:37 +0000

    Fix issue: login social icons color not showing.

--- components/Login/SocialMediaItem.tsx
+++ components/Login/SocialMediaItem.tsx
@@ -18,7 +18,7 @@ const SocialMediaItem = ({ icon: Icon, hex, href }: Props) => {
       onClick={() => router.push(href)}
       className="w-full shadow-md hover:shadow-sm transition-all"
     >
-      <Icon className={`text-[${hex}]`} />
+      <Icon color={hex} />
     </Button>
   );
 };
--- components/Nav/LoginButton.tsx
+++ components/Nav/LoginButton.tsx
@@ -4,21 +4,28 @@ import { useTranslations } from "next-intl";
 import { Button } from "../ui/button";
 import { usePathname, useRouter } from "next/navigation";
 import { getTextDirection } from "@/utils/getTextDirection";
+import { useState } from "react";
 
-const SignUpButton = () => {
+const LoginButton = () => {
   const t = useTranslations();
   const pathname = usePathname();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    router.push(`${pathname}/login`);
+  };
 
   return (
     <Button
       variant="secondary"
       dir={getTextDirection(pathname)}
-      onClick={() => router.push(`${pathname}/login`)}
+      onClick={handleClick}
       className="w-full lg:w-32 lg:h-11 shadow-md bg-background font-bold hover:bg-transparent border-2 border-background hover:text-background"
     >
-      {t("login")}
+      {isLoading ? t("loginButtonLoading") : t("login")}
     </Button>
   );
 };
-export default SignUpButton;
+export default LoginButton;
